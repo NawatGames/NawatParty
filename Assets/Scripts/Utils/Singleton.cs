816700edecc1fc01e75c10b3e73c65e7b7bb0103
Lucas Calçada <lucasmgmcalcada@gmail.com>
2025-04-13 19:18:04 -0300

    fix: remove DontDestroyOnLoad from Singleton class

--- Assets/Scripts/Utils/Singleton.cs
+++ Assets/Scripts/Utils/Singleton.cs
@@ -1,6 +1,3 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
 using UnityEngine;
 
 namespace Utils
@@ -16,7 +13,6 @@ namespace Utils
                 Destroy(this);
             }
             Instance = this as T;
-            DontDestroyOnLoad(this);
         }
     }
 }
