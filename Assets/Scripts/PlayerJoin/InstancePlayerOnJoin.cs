cb1c9ba7b8974cad7c4a79c66c55a1bb4679161c
Lucas Calçada <lucasmgmcalcada@gmail.com>
2025-04-13 23:31:49 -0300

    feat(playerjoin): add InstancePlayerOnJoin

---
+++ Assets/Scripts/PlayerJoin/InstancePlayerOnJoin.cs
@@ -0,0 +1,30 @@
+using Generic;
+using Player;
+using UnityEngine;
+
+namespace PlayerJoin
+{
+    public class InstancePlayerOnJoin : MonoBehaviour
+    {
+        private PlayerManager _playerManager;
+
+        [SerializeField] private GameObject playerPrefab;
+
+        private void OnEnable()
+        {
+            _playerManager = PlayerManager.Instance;
+            _playerManager.onPlayerRegister.AddListener(OnPlayerJoin);
+        }
+
+        private void OnDisable()
+        {
+            _playerManager.onPlayerRegister.RemoveListener(OnPlayerJoin);
+        }
+
+        private void OnPlayerJoin(PlayerInstance player)
+        {
+            GameObject newPlayer = Instantiate(playerPrefab, transform);
+            newPlayer.GetComponent<GenericPlayerMovement>().Initialize(player);
+        }
+    }
+}
