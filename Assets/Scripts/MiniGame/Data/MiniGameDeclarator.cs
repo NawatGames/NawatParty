517174f3a704ca86575ef2eb7b74902ca6590c45
LucasCalcada <lucasmgmcalcada@gmail.com>
2025-04-20 17:10:22 -0300

    feat(main): add description field to minigame declarator

--- Assets/Scripts/MiniGame/Data/MiniGameDeclarator.cs
+++ Assets/Scripts/MiniGame/Data/MiniGameDeclarator.cs
@@ -19,12 +19,15 @@ namespace MiniGame.Data
     {
         // Editor Fields
         [SerializeField] private string gameName;
+        [TextArea(3, 5)]
+        [SerializeField] private string gameDescription;
         [SerializeField] private SceneAsset gameScene;
         [SerializeField] private GameType gameType;
         [SerializeField] private MiniGameInputDeclarator inputDeclarator;
 
         // Getters
         public string GameName => gameName;
+        public string GameDescription => gameDescription;
         public SceneAsset GameScene => gameScene;
         public GameType GameType => gameType;
         public MiniGameInputDeclarator InputDeclarator => inputDeclarator;
