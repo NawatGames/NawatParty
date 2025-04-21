6585c3a6c6a4114ee53207c56e54524e32f2292a
LucasCalcada <lucasmgmcalcada@gmail.com>
2025-04-21 17:16:46 -0300

    fix(utils): fix singleton default execution order

--- Assets/Scripts/Utils/Singleton.cs
+++ Assets/Scripts/Utils/Singleton.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Utils
 {
+    [DefaultExecutionOrder(-100)]
     public class Singleton<T> : MonoBehaviour where T : Component
     {
         public static T Instance { get; private set; }
